6331488f3066bf8002a1308afd5d2955cc66057b
Rafael Oliveira <faelmegga@gmail.com>
2022-08-04 22:51:52 -0300

    Subindo arquivos do site

---
+++ script.js
@@ -0,0 +1,15 @@
+function relatorio1(){
+    var rel = document.querySelector("#relatorio");
+    rel.setAttribute('src', 'https://app.powerbi.com/view?r=eyJrIjoiYjg2ZTc2Y2YtOWNmZS00NjUwLTljNmItMDkwMzNhOGQ0NTg4IiwidCI6ImY3NzVhODAwLTQ4NmItNDkzNS05ZTE3LTBkYWNmYWE5OWRlMSJ9');
+}
+
+function relatorio2(){
+    var rel = document.querySelector("#relatorio");
+    rel.setAttribute('src', 'https://app.powerbi.com/view?r=eyJrIjoiNzdiNDNlOWUtZDFiNC00OTljLTg0NjMtNjEzMzM3N2UzZTlhIiwidCI6ImY3NzVhODAwLTQ4NmItNDkzNS05ZTE3LTBkYWNmYWE5OWRlMSJ9');
+}
+
+function relatorio3(){
+    var rel = document.querySelector("#relatorio");
+    rel.setAttribute('src', 'https://app.powerbi.com/view?r=eyJrIjoiNTJmY2RiZjYtZTQyNi00YzVmLWIxYzAtMjEyODk4YjZhMzAzIiwidCI6ImY3NzVhODAwLTQ4NmItNDkzNS05ZTE3LTBkYWNmYWE5OWRlMSJ9');
+}
+
